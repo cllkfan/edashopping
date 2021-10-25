33c28d3b05eeee5b392c2c2ccd6175926f7c353a
un <2119530470@qq.com>
2021-10-25 10:51:49 +0800

    配置修改

--- src/main/java/eda/controller/UsersController.java
+++ src/main/java/eda/controller/UsersController.java
@@ -3,6 +3,7 @@ package eda.controller;
 import eda.domain.Users;
 import eda.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2021-10-24 23:12
  */
 @RestController
+@RequestMapping("/user")
 public class UsersController {
 
     @Autowired
     private UsersService usersService;
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public String login(String username,String passwd){
+
+        System.out.println(username);
+        System.out.println(passwd);
+
         Users login = usersService.login(username);
         String pwd = login.getPwd();
         if (!pwd.equals(passwd)){
